857319d58433bb07d990496789268997fd444366
softprime200 <51472404+softprime200@users.noreply.github.com>
2019-06-10 22:43:50 -0700

    Initial commit

---
+++ app/src/main/java/net/vrgsoft/videocrop/MainActivity.java
@@ -0,0 +1,30 @@
+package net.vrgsoft.videocrop;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import net.vrgsoft.videcrop.VideoCropActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+    private static final int CROP_REQUEST = 200;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String inputPath = "/storage/emulated/0/WhatsApp/Media/WhatsApp Video/a.mp4";
+//        String inputPath = "/storage/emulated/0/VidMate/download/d.mp4";
+        String outputPath = "/storage/emulated/0/YDXJ0.mp4";
+        startActivityForResult(VideoCropActivity.createIntent(this, inputPath, outputPath), CROP_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == CROP_REQUEST && resultCode == RESULT_OK){
+            //crop successful
+        }
+    }
+}
